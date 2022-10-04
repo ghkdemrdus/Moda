f4eb1d066e1b7b208734264f5cc919570412d747
deukyeon <ghkdemrdus@gmail.com>
2022-10-04 14:19:33 +0900

    first commit

---
+++ moda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moda
+//
+//  Created by 황득연 on 2022/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
